0f7a512c80ba5f55dbd5dd9045636c3a43b38298
Fikadu Buzuneh <40325091+fekadum@users.noreply.github.com>
2025-03-23 17:38:47 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
-
+import Main from './components/main'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Header/>
+    <Main/>
     </>
   )
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+    return (
+        <main>
+            <form className="">
+                <input type="text" placeholder="eg. oregano"/>
+                <button>Add Ingredient</button>
+            </form>
+        </main>
+    )
+}
