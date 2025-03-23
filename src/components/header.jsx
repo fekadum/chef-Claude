68cd05faa1fc8b7a911ac64cf34c7e8e95312a4b
Fikadu Buzuneh <40325091+fekadum@users.noreply.github.com>
2025-03-23 16:08:04 +0300

    project setup and header

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <div className="header-container">
+            <img src="" alt="" />
+            <h1>Chef Claudes</h1>
+        </div>
+    )
+}
