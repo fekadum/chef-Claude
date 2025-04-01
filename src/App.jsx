5f89d8aa266e490742e9f8ab8837832846ec1dd9
Fikadu Buzuneh <FikaduB@ethiopianairlines.com>
2025-04-01 08:52:49 +0300

    add sample apis and token

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
 import Main from './components/main'
+import { HfInference } from '@huggingface/inference'
+import {getRecipeFromMistral} from './components/hugface'
 function App() {
   const [count, setCount] = useState(0)
 
+  console.log('Full Env:', import.meta.env);
+ console.log('api key',import.meta.env.VITE_HUGFACE_KEY);
+ console.log("key name", import.meta.env); // Should list all environment variables
+ const hf = new HfInference(import.meta.env.VITE_HUGFACE_KEY)
+ async  function command(params) {
+var res= await getRecipeFromMistral()
+ }
   return (
     <>
     <Header/>
     <Main/>
+    <button onClick={command}></button>
     </>
   )
 }
